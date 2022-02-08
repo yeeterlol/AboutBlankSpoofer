117058638b725e31d41f52363260a8581544e973
yeeterlol <81702700+yeeterlol@users.noreply.github.com>
2022-02-08 15:08:33 -0800

    Fix

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-let url = prompt("about:blank spoofer | what website do you want to spoof");
-
-win = window.open();
-win.document.body.style.margin = '0';
-win.document.body.style.height = '100vh';
-var iframe = win.document.createElement('iframe');
-iframe.style.border = 'none';
-iframe.style.width = '100%';
-iframe.style.height = '100%';
-iframe.style.margin = '0';
-iframe.src = url;
-win.document.body.appendChild(iframe);
+// about:blank spoofer
+// made by github.com/yeeterlol
+(function() {
+    let url = prompt("about:blank spoofer | what website do you want to spoof");
+    win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+})();
